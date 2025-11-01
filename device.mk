263fad00ea881028265ba6c2c89d6d093d92c568
Rocky7842 <eric.rocky7842@gmail.com>
2025-11-01 19:18:10 +0800

    lisa: Build MIUI camera
    Change-Id: I35b026e5a622593bc37fae6ec9a184707e33eb2a

--- device.mk
+++ device.mk
@@ -34,6 +34,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_yupikqrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/sound_trigger_mixer_paths_yupikqrd.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/sound_trigger_platform_info.xml
 
+# Camera (MIUI)
+$(call inherit-product-if-exists, vendor/xiaomi/camera-lisa/device-include.mk)
+
 # Display
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,qdcm_calib_data_*.xml,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc/)
